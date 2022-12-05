a7c84811d5b850aa5b6fd5ea99a3c68a19110e88
Fredrick Simi <fkemosi@gmail.com>
2022-12-05 12:44:32 +0300

    changes

--- app/myapp/volustar/settings.py
+++ app/myapp/volustar/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'volustar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DBNAME'),
+        'USER': config('DBUSER'),
+        'PASSWORD': config('DBPASSWORD'),
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
